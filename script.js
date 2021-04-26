782b509c04ca1891cf3b672abace5b742378efbe
Melody <yu.musicssis@gmail.com>
2021-04-27 00:15:38 +0800

    changed some styling

--- script.js
+++ script.js
@@ -4,13 +4,12 @@ var markComplete = function (id){
         url: 'https://altcademy-to-do-list-api.herokuapp.com/tasks/'+ id + '/mark_complete?api_key=53',
         dataType: 'json',
         success: function (response, textStatus) {
-            displayAllTasks();
+            //displayAllTasks();
         },
         error: function (request, textStatus, errorMessage) {
             console.log(errorMessage);
         }
-    }); 
-       
+    });  
 }
 
 var markActive = function (id) {
@@ -19,14 +18,12 @@ var markActive = function (id) {
         url: 'https://altcademy-to-do-list-api.herokuapp.com/tasks/' + id + '/mark_active?api_key=53',
         dataType: 'json',
         success: function (response, textStatus) {
-        displayAllTasks();
-        console.log(this);
+        //displayAllTasks();
       },
       error: function (request, textStatus, errorMessage) {
         console.log(errorMessage);
       }
     });
-
   }
 
 var createTaskAjax = function (){
@@ -55,32 +52,34 @@ var displayAllTasks = function () {
       url: 'https://altcademy-to-do-list-api.herokuapp.com/tasks?api_key=53',
       dataType: 'json',
       success: function (response, textStatus) {
+        getDateNow();
         $('tbody').empty();
         response.tasks.forEach(function (task) {
         var taskName = task.content;
         var id = task.id;
         
-        $('tbody').append('<tr class = "task-row">' +
+        $('tbody').append('<tr class = "task-row activeTask">' +
         '<td> <input type="checkbox" class = "check" data-id = "'+ id + '"' + (task.completed ? 'checked' : '') + 
         '>'+
         '<td class="task">' + taskName + '</td>' +
         '<td><button class="btn btn-light btn-sm remove" data-id = "'+ id + '"> remove</button></td>' +
     '</tr>');
-
         })
-
-        
       },
       error: function (request, textStatus, errorMessage) {
         console.log(errorMessage);
       }
     });
-}
+  }
+
+  var getDateNow = function() {
+    var date = new Date();
+    document.getElementById("dateNow").innerHTML = ("Date: " + date.getFullYear() + "/" + date.getMonth() + "/" + date.getDate());
+  }
 
 $(document).ready(function () {
 
     displayAllTasks(); 
-
     $(document).on('click', '.btn.remove', function (event) {
         var idTask = $(this).data('id');
         $.ajax({
@@ -94,66 +93,58 @@ $(document).ready(function () {
              }
            });
         $(this).closest('tr').remove();
-
     });
 
     $('#add-task').on('submit', function (event) {
         event.preventDefault();
-        console.log(event);
-        console.log(this);
         createTaskAjax();
-        $('#todo-input').val('');
-        //if there is a checkbox, then change the class
-        /*if($(input[checked])){
-          $('tr').addClass('complete');
-          $('tr').removeClass('active');
-        }else{
-          $('tr').removeClass('complete');
-          $('tr').addClass('active');
-        }*/
-        
+        $('#todo-input').val('');        
     });
 
     $(document).on('change', '.check', function(event) {
       var idTask = $(this).data('id');
       console.log(this);
       if(this.checked) {
-          markComplete(idTask);
-          
+          markComplete(idTask);     
           console.log((this).closest('tr'));
-          $(this).closest('tr').addClass('complete');
-          event.preventDefault();
-          
-          
+          $(this).closest('tr').removeClass('activeTask');
+          $(this).closest('tr').addClass('completeTask');   
       } else {
           markActive(idTask);
+          console.log((this).closest('tr'));
           $(this).closest('tr').show();
-          $(this).closest('tr').addClass('active');
-          event.preventDefault();
+          $(this).closest('tr').addClass('activeTask');
+          $(this).closest('tr').removeClass('completeTask');
       }    
   });
 
-    $(document).on('change', '.all-filter', function(event){
-      $('tr').toggle();
+    $(document).on('click', '#all-filter', function(event){
+      console.log("clicked all");
+      $('label').removeClass('active');
+      $(this).closest('label').addClass('active');
+      $('tr').show();
     })
 
-    $(document).on('change', '.active-filter', function(event){
-      
-     /* if($("input[type = 'checkbox']").getAttribute( "checked")){
-        console.log("it's checked")
-        //$(this).closest('tr').hide();
-      } else {
-        $(this).closest('tr').show();
-      } */
-      
-      $('tr .complete').hide();
-      $('tr .active').show();
+    $(document).on('click', '#active', function(event){
+      $('label').removeClass('active');
+      $(this).closest('label').addClass('active');
+      $('.task-row.completeTask').hide();
+      $('.task-row.activeTask').show();
+      event.preventDefault();
     })
 
-    /*$(document).on('change', '.complete-filter', function(event){
-      $(input['checked']).show();
-      !($(input['checked'])).hide();
-  */
-    
+    $(document).on('click', '#complete', function(event){
+
+      $('label').removeClass('active');
+      $(this).closest('label').addClass('active');
+      //$('.activeTask').hide(100);
+      $('.task-row.activeTask').hide();
+      $('.task-row.completeTask').show();
+      event.preventDefault();
+      
+      
+    })
+
+
 });
 
